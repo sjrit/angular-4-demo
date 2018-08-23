0a23bcfe505259ca8411a1bfb3949d176771d01a
irajmrperfect <i.rajmrperfect@gmail.com>
2018-08-23 19:07:21 +0530

    first commit

---
+++ server/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    database:
+        'mongodb://rajdubey:raj123@ds121311.mlab.com:21311/amazono',
+    port: 3030,
+    secret: 'SJRITAMAZONOAPP123'
+};
---
+++ server/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const config = require('./config');
+
+const app = express();
+
+// Only include useMongoClient only if your mongoose version is < 5.0.0
+mongoose.connect(config.database,err => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Connected to the database');
+  }
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(morgan('dev'));
+app.use(cors());
+
+const userRoutes = require('./routes/account');
+
+app.use('/api/accounts', userRoutes);
+
+
+app.listen(config.port, err => {
+  console.log('Magic happens on port awesome ' + config.port);
+});
